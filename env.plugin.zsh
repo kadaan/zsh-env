3e719977560642c1dca6c5608e774583d654ccdc
Joel Baranick <joel.baranick@ensighten.com>
2020-10-16 14:24:28 -0700

    Add env unsetting

--- env.plugin.zsh
+++ env.plugin.zsh
@@ -4,7 +4,8 @@ chenv() {
   local CONF=$(find $DIR -maxdepth 1 -name $PATTERN -print -quit)
   if test -n "$CONF"; then
       source $CONF
-      export $(egrep -v '^[[:blank:]]*#|^[[:blank:]]*$' "$CONF" | cut -d= -f1)
+      export $(egrep -v '^[[:blank:]]*#|^[[:blank:]]*$' "$CONF" | egrep -v '^.*=$' | cut -d= -f1)
+      unset $(egrep -v '^[[:blank:]]*#|^[[:blank:]]*$' "$CONF" | egrep '^.*=$' | cut -d= -f1)
   else
       echo "Environment '$1' does not exist" 1>&2
       return 1
